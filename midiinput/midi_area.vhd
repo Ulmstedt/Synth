cadd04e090b9cd4f011dad1b572b60201649d455
Mattias <matte___93@hotmail.com>
2015-03-31 15:42:27 +0200

    MIDI input working

--- midiinput/midi_area.vhd
+++ midiinput/midi_area.vhd
@@ -30,37 +30,39 @@ end component;
 
 component CheckMsg is
    port(
-      clk      : in std_logic; -- Clock
-      rst      : in std_logic; -- Reset
-      msgReady : in std_logic; -- 1 when there is a msg ready in tmpReg
-      outReady : in std_logic; -- 1 when message is ready to be transfered from tmpReg to mreg#
-      muxCtr   : in std_logic_vector(1 downto 0);
-      tmpReg   : in std_logic_vector(MIDI_WIDTH - 1 downto 0);
-      mreg1    : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
-      mreg2    : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
-      mreg3    : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
-      validMsg : out std_logic; -- 1 when we have a valid incoming midi-msg
-      readRdy  : out std_logic -- pulse when a complete message is ready in Mreg1-3
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      msgReady          : in std_logic; -- 1 when there is a msg ready in tmpReg
+      outReady          : in std_logic; -- 1 when message is ready to be transfered from tmpReg to mreg#
+      muxCtr            : in std_logic_vector(1 downto 0);
+      tmpReg            : in std_logic_vector(MIDI_WIDTH - 1 downto 0);
+      mreg1             : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
+      mreg2             : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
+      mreg3             : out std_logic_vector(MIDI_WIDTH - 1 downto 0);
+      validMsgO         : out std_logic; -- 1 when we have a valid incoming midi-msg
+      msgReadyDelayed   : out std_logic; -- A msgReady pulse that is delayed by 1 ck
+      readRdy           : out std_logic -- pulse when a complete message is ready in Mreg1-3
    );
 end component;
 
 component MuxCounter is
    port(
-      clk      : in std_logic; -- Clock
-      rst      : in std_logic; -- Reset
-      validMsg : in std_logic; -- 1 when the message in tmpReg is valid
-      msgReady : in std_logic; -- 1 when a message is ready in tmpReg
-      muxCtr   : out std_logic_vector(1 downto 0); -- MUX counter
-      outReady : out std_logic -- 1 when the message is ready to be transfered from tmpReg to mreg#
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      validMsgI         : in std_logic; -- 1 when the message in tmpReg is valid
+      msgReadyDelayed   : in std_logic; -- 1 when a message is ready in tmpReg
+      muxCtr            : out std_logic_vector(1 downto 0); -- MUX counter
+      outReady          : out std_logic -- 1 when the message is ready to be transfered from tmpReg to mreg#
    );
 end component;
 
 
-signal tmpRegS    : std_logic_vector(MIDI_WIDTH - 1 downto 0);
-signal msgReady   : std_logic;
-signal outReady   : std_logic;
-signal muxCtr     : std_logic_vector(1 downto 0);
-signal validMsg   : std_logic;
+signal tmpRegS          : std_logic_vector(MIDI_WIDTH - 1 downto 0);
+signal msgReady         : std_logic;
+signal msgReadyDelayedS : std_logic;
+signal outReady         : std_logic;
+signal muxCtr           : std_logic_vector(1 downto 0);
+signal validMsg         : std_logic;
 
 begin
 
@@ -76,21 +78,22 @@ begin
       clk => clk, 
       rst => rst,
       msgReady => msgReady,
+      msgReadyDelayed => msgReadyDelayedS,
       outReady => outReady,
       muxCtr => muxCtr,
       tmpReg => tmpRegS,
       mreg1 => mreg1,
       mreg2 => mreg2,
       mreg3 => mreg3,
-      validMsg => validMsg,
+      validMsgO => validMsg,
       readRdy => readRdy
    );
    
    midiMuxCounter  : MuxCounter port map(
       clk => clk, 
       rst => rst,
-      validMsg => validMsg,
-      msgReady => msgReady,
+      validMsgI => validMsg,
+      msgReadyDelayed => msgReadyDelayedS,
       muxCtr => muxCtr,
       outReady => outReady
    );
--- midiinput/midi_mux_counter.vhd
+++ midiinput/midi_mux_counter.vhd
@@ -6,12 +6,12 @@ use work.midi_constants.all;
 
 entity MuxCounter is
    port(
-      clk      : in std_logic; -- Clock
-      rst      : in std_logic; -- Reset
-      validMsg : in std_logic; -- 1 when the message in tmpReg is valid
-      msgReady : in std_logic; -- 1 when a message is ready in tmpReg
-      muxCtr   : out std_logic_vector(1 downto 0); -- MUX counter
-      outReady : out std_logic -- 1 when the message is ready to be transfered from tmpReg to mreg#
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      validMsgI         : in std_logic; -- 1 when the message in tmpReg is valid
+      msgReadyDelayed   : in std_logic; -- 1 when a message is ready in tmpReg
+      muxCtr            : out std_logic_vector(1 downto 0); -- MUX counter
+      outReady          : out std_logic -- 1 when the message is ready to be transfered from tmpReg to mreg#
    );
 end MuxCounter;
 
@@ -22,46 +22,41 @@ signal outReadyS  : std_logic := '0';
 
 begin
 
-	process(clk) is
-	begin
-	   if rising_edge(clk) and rst = '0' then
-		  if validMsg = '1' and msgReady = '1' then
-			 case count is
-				when "00" =>
-							count <= "01";
-							outReadyS <= '1';
-				when "01" =>
-							count <= "10";
-							outReadyS <= '1';
-				when "10" =>
-							count <= "00";
-							outReadyS <= '1';
-				when others => null;
-			 end case;
-		  elsif validMsg = '0' then
-			 count <= "00";
-		  end if;                 
-	   end if;
-	end process;
+   process(clk) is
+   begin
+      if rising_edge(clk) then
+         if rst = '1' then
+            count <= "00";
+         else
+         
+           if validMsgI = '1' and msgReadyDelayed = '1' then
+               case count is
+                  when "00" =>
+                           count <= "01";
+                           outReadyS <= '1';
+                  when "01" =>
+                           count <= "10";
+                           outReadyS <= '1';
+                  when "10" =>
+                           count <= "11";
+                           outReadyS <= '1';
+                  when others => null;
+               end case;
+            elsif validMsgI = '0' then
+               count <= "00";
+            end if;
+           
+            -- Out ready should only be 1 for 1 clk
+            if outReadyS = '1' then
+               outReadyS <= '0';
+            end if;
+            
+         end if;
+      end if;
+   end process;
 
-	muxCtr <= count;
+   muxCtr <= count;
+   outReady <= outReadyS;
 
-	-- Out ready should only be 1 for 1 clk
-	process(clk) is
-	begin
-	   if rising_edge(clk) and outReadyS = '1' then
-		  outReadyS <= '0';
-	   end if;
-	end process;
-
-	outReady <= outReadyS;
-
-	-- Reset
-	process(clk) is
-	begin
-	   if rising_edge(clk) and rst = '1' then
-		  count <= "00";
-	   end if;
-	end process;
 
 end Behavioral;
