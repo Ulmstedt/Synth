0c26d36b6b0805c9e6a1e7ca8ec1c83f275effeb
Joacim S <eletexel@hotmail.com>
2015-03-31 11:13:52 +0200

    new

--- cpu/alutb.vhd
+++ cpu/alutb.vhd
@@ -14,48 +14,48 @@ ARCHITECTURE behavior OF bus_tb IS
   -- Component Declaration
 Component ALU is
    port(
-      left_In  : in std_logic_vector(REG_WIDTH-1 downto 0);
-      right_In : in std_logic_vector(REG_WIDTH-1 downto 0);
-      alu_Out  : out std_logic_vector(REG_WIDTH-1 downto 0);
-      ALU_instr  : in std_logic_vector(4 downto 0);
+      leftIn  : in std_logic_vector(REG_WIDTH-1 downto 0);
+      rightIn : in std_logic_vector(REG_WIDTH-1 downto 0);
+      ALUOut  : out std_logic_vector(REG_WIDTH-1 downto 0);
+      ALUInstr: in std_logic_vector(4 downto 0);
       
-      clk      : in std_logic;
+      clk     : in std_logic;
       
-      sr_Z     : out std_logic;
-      sr_N     : out std_logic;
-      sr_O     : out std_logic;
-      sr_C     : out std_logic
+      sRZ     : out std_logic;
+      sRN     : out std_logic;
+      sRO     : out std_logic;
+      sRC     : out std_logic
    );
 end Component;
 	
    SIGNAL clk 		   : std_logic	:= '0';
    SIGNAL rst 		   : std_logic	:= '0';
-   signal left_In    : std_logic_vector(REG_WIDTH-1 downto 0) := "1111" & "1111" & "1111" & "1111" ;
-   signal right_In   : std_logic_vector(REG_WIDTH-1 downto 0) := "1111" & "1111" & "0000" & "1010";
-   signal alu_Out    : std_logic_vector(REG_WIDTH-1 downto 0);
-   signal ALU_instr  : std_logic_vector(4 downto 0) := "01000";
+   signal leftIn    : std_logic_vector(REG_WIDTH-1 downto 0) := "1111" & "1111" & "1111" & "1111" ;
+   signal rightIn   : std_logic_vector(REG_WIDTH-1 downto 0) := "1111" & "1111" & "0000" & "1010";
+   signal ALUOut    : std_logic_vector(REG_WIDTH-1 downto 0);
+   signal ALUInstr  : std_logic_vector(4 downto 0) := "01000";
       
-   signal sr_Z       : std_logic;
-   signal sr_N       : std_logic;
-   signal sr_O       : std_logic;
-   signal sr_C       : std_logic;
+   signal sRZ       : std_logic := '0';
+   signal sRN       : std_logic := '0';
+   signal sRO       : std_logic := '0';
+   signal sRC       : std_logic := '0';
    signal tb_running : boolean 	:= true;
 
 BEGIN
 
   -- Component Instantiation
    alut : ALU port map(
-      left_In  => left_In,
-      right_In => right_In,
-      alu_Out  => alu_Out,
-      ALU_instr=> ALU_instr,
+      leftIn  => leftIn,
+      rightIn => rightIn,
+      ALUOut  => ALUOut,
+      ALUInstr=> ALUInstr,
       
       clk      => clk,
       
-      sr_Z     => sr_Z,
-      sr_N     => sr_N,
-      sr_O     => sr_O,
-      sr_C     => sr_C
+      sRZ     => sRZ,
+      sRN     => sRN,
+      sRO     => sRO,
+      sRC     => sRC
    );
    
    
