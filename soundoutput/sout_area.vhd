8277369691b9f5ef6904fb851fce89839dfdaab2
Mattias <matte___93@hotmail.com>
2015-04-16 12:27:54 +0200

    Sound testing

---
+++ soundoutput/sout_area.vhd
@@ -0,0 +1,63 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+use work.sout_constants.all;
+
+entity SoutArea is
+   port(
+      clk               : in std_logic;
+      rst               : in std_logic;
+      sampleBuffer      : in std_logic_vector(SAMPLE_SIZE - 1 downto 0);
+      sclk              : out std_logic; -- Serial clock
+      mclk              : out std_logic; -- Master clock
+      lrck              : out std_logic; -- Left/Right clock
+      sdout             : out std_logic -- Serial data output
+   );
+end SoutArea;
+
+architecture Behavioral of SoutArea is
+
+component SoundOutput is
+   port(
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      sampleBuffer      : in std_logic_vector(SAMPLE_SIZE - 1 downto 0);
+      sclk              : in std_logic; -- Serial clock
+      lrck              : out std_logic; -- Left/Right clock
+      sdout             : out std_logic -- Serial data output
+   );
+end component;
+
+component SoutClkgen is
+   port(
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      sclk              : out std_logic; -- Serial clock
+      mclk              : out std_logic -- Master clock
+   );
+end component;
+
+signal sclkS            : std_logic;
+
+
+begin
+
+   sound_output  : SoundOutput port map(
+      clk => clk, 
+      rst => rst,
+      sampleBuffer => sampleBuffer,
+      sclk => sclkS,
+      lrck => lrck,
+      sdout => sdout
+   );
+
+   sound_clkgen  : SoutClkgen port map(
+      clk => clk, 
+      rst => rst,
+      sclk => sclkS,
+      mclk => mclk
+   );
+   
+sclk <= sclkS;
+end Behavioral;
---
+++ soundoutput/sout_clkgen.vhd
@@ -0,0 +1,39 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+use work.sout_constants.all;
+
+entity SoutClkgen is
+   port(
+      clk               : in std_logic; -- Clock
+      rst               : in std_logic; -- Reset
+      sclk              : out std_logic; -- Serial clock
+      mclk              : out std_logic -- Master clock
+   );
+end SoutClkgen;
+
+
+architecture Behavioral of SoutClkgen is
+   
+   signal sclkS            : std_logic; -- Serial clock signal
+   signal mclkS            : std_logic; -- Master clock signal
+   
+begin
+
+   process(clk) is
+   begin
+      if rising_edge(clk) then
+         if rst = '1' then
+            sclkS <= '0';
+            mclkS <= '0';
+         else
+            -- Generate clocks
+         end if;
+      end if;
+   end process;
+
+   mclk <= mclkS;
+   sclk <= sclkS;
+   
+end Behavioral;
