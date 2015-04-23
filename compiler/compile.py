63c8750f93b1a23f2775c330239b79897e41ddce
Mattias <matte___93@hotmail.com>
2015-04-23 13:06:48 +0200

    Compiler done

--- compiler/compile.py
+++ compiler/compile.py
@@ -1,17 +1,26 @@
 from __future__ import print_function
 import xml.etree.ElementTree as ET
 
+# Constants dict
+constants = {} 
+
 # Parses and argument and returns its correct form (hex, dec, bin)
 def parse_arg(arg):
+   # Hex
    if arg[0] == '$':
       return bin(int(arg[1:],16))[2:]
+   # Bin
    elif arg[0] == '%':
       return arg[1:]
+   elif arg[0] == '#':
+      return parse_arg(constants[arg[1:]])
+   # Dec
    else:
       return bin(int(arg,10))[2:]
 
 # Compiles the given files to binary format
 def comp_file(*filenames):
+   outfile = open("binary_out", "wb+")
    for curfile in filenames:
       f = open(curfile,'r')
       code = f.readlines()
@@ -21,7 +30,7 @@ def comp_file(*filenames):
 
       # for finding errors
       instr_counter = 0
-      instr_found = True
+      instr_found = True     
 
       for instruction in code:
          if instruction == '\n':
@@ -30,8 +39,14 @@ def comp_file(*filenames):
             print("Bad instruction found on line", instr_counter)
          instr_found = False
          instr_counter += 1
-         instruction = instruction.split('#',1)[0] # Remove eventual comments
+         instruction = instruction.split(';',1)[0] # Remove eventual comments
          instr_list = instruction.upper().replace(',',' ').split() # split string, instr_list[1] = arg1 and so on
+
+         # Constants
+         if instr_list[0].upper() == 'CONSTANT':
+            constants[instr_list[1].upper()] = instr_list[2].upper()
+            instr_found = True
+            continue
          for instr in root.findall('instr'):
             if instr.get('name') == instr_list[0]: # instr is now the needed element
                instr_found = True
@@ -40,6 +55,7 @@ def comp_file(*filenames):
                if len(instr_list) == 1:
                   tempstring = OP + (32-len(OP))*'0'
                   print(tempstring)
+                  outfile.write(tempstring)
                # 2 arguments
                elif len(instr_list) >= 3:
                   DEST_LENGTH = instr.find('DEST').find('LENGTH').text
@@ -59,6 +75,7 @@ def comp_file(*filenames):
                   # Make sure the length of instruction is 32
                   tempstring = OP + ARG1 + (32-len(OP)-int(DEST_LENGTH)-int(SRC_LENGTH)-int(OFF_LENGTH))*'0' + ARG3 + ARG2
                   print(tempstring)
+                  outfile.write(tempstring)
 
                break # Correct instruction has been found
          
