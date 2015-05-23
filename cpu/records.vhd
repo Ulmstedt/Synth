f935ba8a285e7c269d856ca89b9f2851abff7fb4
Joacim S <eletexel@hotmail.com>
2015-05-23 13:02:06 +0200

    tilemap independant

--- cpu/records.vhd
+++ cpu/records.vhd
@@ -23,5 +23,6 @@ package Records is
 	
    type pmem_t is array (0 to PMEM_HEIGHT - 1) of std_logic_vector(PMEM_WIDTH - 1 downto 0);
    type mem_t is array (0 to MEM_HEIGHT - 1) of std_logic_vector(REG_WIDTH - 1 downto 0);
+   type tilemap_t is array (0 to TILE_MAP_HEIGHT - 1) of std_logic_vector(REG_WIDTH - 1 downto 0);
 end Records;
 
